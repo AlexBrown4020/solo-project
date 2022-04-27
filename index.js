f14f5baa959dc48b17a319663fde16e1372a75f3
AlexBrown4020 <balex3497@gmail.com>
2022-04-27 12:20:48 +0900

    feat: Implement query, get Actor by name

--- index.js
+++ index.js
@@ -1,1 +1,91 @@
+const { ApolloServer, gql } = require('apollo-server');
 
+const typeDefs = gql`
+
+scalar Date
+
+type Event {
+    date: Date!
+    description: String
+}
+
+type Actor {
+    name: String!
+    country: String
+}
+
+type Query {
+    Events: [Event]
+    Actors: [Actor]
+    Actor(name:String): Actor
+}
+
+# type Mutation {
+
+# }
+
+`;
+const events = [
+        {
+            date: "14/02/2022",
+            description: "CCP4 started Code Chrysalis."
+        },
+        {
+            date: "19/04/2022",
+            description: "CCP4 started their laundry week."
+        },
+        {
+            date: "26/04/2022",
+            description: "CCP4 started their solo projects."
+        },
+    ];
+const actors = [
+        {
+            name: "Alex",
+            country: "Japan"
+        },
+        {
+            name: "Eric",
+            country: "United States"
+        },
+        {
+            name: "JP",
+            country: "United States"
+        },
+        {
+            name: "Matt",
+            country: "Japan"
+        },
+        {
+            name: "May",
+            country: "Japan"
+        },
+        {
+            name: "Ravi",
+            country: "South Korea"
+        },
+        {
+            name: "Yuka",
+            country: "Japan"
+        }
+    ];
+
+const resolvers = {
+    Query: {
+        Events: () => events,
+        Actors: () => actors,
+        Actor: (parent, args) => {
+            for (let object of actors) {
+                if (object.name === args.name) return object
+            }     
+        }
+    }
+};
+
+const PORT = process.env.PORT || 4000;
+
+const server = new ApolloServer({ resolvers, typeDefs });
+
+server.listen(PORT, () => {
+    console.log(`Server ready at ${PORT}`)
+});
