218f2bbcc8517437e3baf35d74ff25d95fe12c97
AlexBrown4020 <balex3497@gmail.com>
2022-04-29 17:38:25 +0900

    fix: work in progress for test files

--- test/spec.js
+++ test/spec.js
@@ -1,7 +1,11 @@
 const expect = chai.expect;
+const chaiHttp = require('chai-http');
+const { db } = require('./knex')
+
 
 describe('Database', function () {
     it('actors should exist', function () {
+        console.log(db)
         expect(1).to.equal(2)
     })
 });
