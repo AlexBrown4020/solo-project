2af6f1dc61734df9a66e7f3dbd4ca7988b3eda0d
AlexBrown4020 <balex3497@gmail.com>
2022-04-30 16:28:52 +0900

    Refactor: Change readme

---
+++ db/seeds/involvement-seed-data.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('involvement').del()
+  await knex('involvement').insert([
+    {actor_id: 1, event_title: "First day of Code Chrysalis"},
+    {actor_id: 2, event_title: "First day of Code Chrysalis"},
+    {actor_id: 3, event_title: "First day of Code Chrysalis"},
+    {actor_id: 4, event_title: "First day of Code Chrysalis"},
+    {actor_id: 5, event_title: "First day of Code Chrysalis"},
+    {actor_id: 6, event_title: "First day of Code Chrysalis"},
+    {actor_id: 1, event_title: "Complete foundations course"},
+  ]);
+};
