9429cf57f5928267e7f3d92872f834b05f35c59b
AlexBrown4020 <balex3497@gmail.com>
2022-05-20 12:56:12 +0900

    Feat: Change index.js for apollo/heroku migration

--- index.js
+++ index.js
@@ -7,22 +7,20 @@ const app = require('./app.js');
 
 const PORT = process.env.PORT || 4000;
 
-
-
-(async () => {
-  try {
-    console.log("Running migrations");
-    await db.migrate.latest();
-    console.log("Seeding data");
-    await db.seed.run();
-
-    const server = new ApolloServer({ typeDefs, resolvers, csrfPrevention: true });
-    console.log("Starting express");
-    await server.start();
-    server.applyMiddleware({ app, path: "/" });
-    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
-  } catch (err) {
-    console.error("Error starting app!", err);
-    process.exit(-1);
-  }
-})();
+  (async () => {
+    try {
+      console.log("Running migrations");
+      await db.migrate.latest();
+      console.log("Seeding data");
+      await db.seed.run();
+  
+      const server = new ApolloServer({ typeDefs, resolvers, csrfPrevention: true, });
+      console.log("Starting express");
+      await server.start();
+      server.applyMiddleware({ app, path: "/" });
+      app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+    } catch (err){
+      console.error("Error starting app!", err);
+      process.exit(-1);
+    }
+  })();
