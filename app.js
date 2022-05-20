297e4ba929711ae454e6a0d2c4d42174a0fcdfab
AlexBrown4020 <balex3497@gmail.com>
2022-05-20 14:49:37 +0900

    Fix:Send server info to apollo

--- app.js
+++ app.js
@@ -4,28 +4,30 @@ const morgan = require('morgan');
 const path = require('path');
 const db = require('./knex');
 
-const app = express();
+const serverSetup = () => {
+    const app = express();
+    app.use(
+        morgan(
+            ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'
+        )
+    );
+    
+    app.use(express.static(path.resolve(__dirname, '..', 'dist')));
+    app.use(cors())
+    app.get('/api/events', async (req, res) => {
+        try {
+            const events = await db.select().table('events');
+            res.json(events);
+        } catch (err) {
+            console.error('Error loading events', err);
+            res.sendStatus(500);
+        }
+    });
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'))
+    });
+    return app; //configured instance of express
+}
 
-app.use(
-    morgan(
-        ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'
-    )
-);
-
-app.use(express.static(path.resolve(__dirname, '..', 'dist')));
-app.use(cors())
-app.get('/api/events', async (req, res) => {
-    try {
-        const events = await db.select().table('events');
-        res.json(events);
-    } catch (err) {
-        console.error('Error loading events', err);
-        res.sendStatus(500);
-    }
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'))
-});
-
-module.exports = app;
+module.exports = serverSetup;
--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('apollo-server-express');
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
-const app = require('./app')
-
+const serverSetup = require('./app');
+const app = serverSetup();
 const PORT = process.env.PORT || 4000;
 
-const server = new ApolloServer({ 
+(async () => {
+  const server = new ApolloServer({ 
     resolvers, 
     typeDefs,
   });
-server.applyMiddleware({ app })
-server.listen(PORT, () => {
-    console.log(`Server ready at port ${PORT}`);
-});
+  await server.start();
+  server.applyMiddleware({ app })
+  app.listen(PORT, () => {
+      console.log(`Server ready at port ${PORT}`);
+  });
+})();
+
+
+// (async () => {
+//   try {
+//     console.log("Running migrations");
+//     await db.migrate.latest();
+//     console.log("Seeding data");
+//     await db.seed.run();
+
+//     const server = new ApolloServer({ typeDefs, resolvers });
+//     console.log("Starting express");
+//     await server.start();
+//     server.applyMiddleware({ app, path: "/" });
+//     app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+//   } catch (err) {
+//     console.error("Error starting app!", err);
+//     process.exit(-1);
+//   }
+// })();
