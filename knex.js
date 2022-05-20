9b2de3a5677dc7d0e5b16ad2e1efb1b713eb21ce
AlexBrown4020 <balex3497@gmail.com>
2022-05-20 09:04:21 +0900

    Fix: revert issues

--- knex.js
+++ knex.js
@@ -1,6 +1,4 @@
 const config = require('./knexfile');
-const knex = require('knex');
+const knex = require('/knex')(config);
 
-const db = knex(config.development)
-
-module.exports = db;
+module.exports = knex;
