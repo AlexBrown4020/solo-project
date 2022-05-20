c6091dd974b5484da765c0030e78895f9f572745
AlexBrown4020 <balex3497@gmail.com>
2022-05-20 21:21:47 +0900

    Feat: Require vue/cli package

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+      <h1>Welcome to Timeline</h1>
+      <HelloWorld/>
+      <p>{{this.post}}</p>
+  </div>
+</template>
+
+<script>
+
+import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      post: null,
+    };
+  },
+  created: async function() {
+    await axios
+    .get("https://new-timeline.herokuapp.com/graphql/events")
+    .then(result => {
+      this.post = result.data;
+      console.log(result.data)
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
