b51b365acaa2bacbe7da8e6951e11a8ee5f58c21
AlexBrown4020 <balex3497@gmail.com>
2022-05-20 12:23:38 +0900

    Feat: Implement apollo server requirements

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('apollo-server-express');
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
+const db = require('./knex');
+
+const app = require('./app.js');
 
 const PORT = process.env.PORT || 4000;
 
-const server = new ApolloServer({ 
-    resolvers, 
-    typeDefs,
-    csrfPrevention: true,
-  });
 
-server.listen(PORT, () => {
-    console.log(`Server ready at port ${PORT}`);
-});
+
+(async () => {
+  try {
+    console.log("Running migrations");
+    await db.migrate.latest();
+    console.log("Seeding data");
+    await db.seed.run();
+
+    const server = new ApolloServer({ typeDefs, resolvers, csrfPrevention: true });
+    console.log("Starting express");
+    await server.start();
+    server.applyMiddleware({ app, path: "/" });
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+  } catch (err) {
+    console.error("Error starting app!", err);
+    process.exit(-1);
+  }
+})();
