454569a1d96d099363d265afbb5bead9f0cbfb08
AlexBrown4020 <balex3497@gmail.com>
2022-05-20 21:56:58 +0900

    Fix:Solve heroku push error, can't find dist/index

--- index.js
+++ index.js
@@ -19,22 +19,3 @@ const db = require('./knex');
       console.log(`Server ready at port ${PORT}`);
   });
 })();
-
-
-// (async () => {
-//   try {
-//     console.log("Running migrations");
-//     await db.migrate.latest();
-//     console.log("Seeding data");
-//     await db.seed.run();
-
-//     const server = new ApolloServer({ typeDefs, resolvers });
-//     console.log("Starting express");
-//     await server.start();
-//     server.applyMiddleware({ app, path: "/" });
-//     app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
-//   } catch (err) {
-//     console.error("Error starting app!", err);
-//     process.exit(-1);
-//   }
-// })();
